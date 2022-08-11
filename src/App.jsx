555e96ca7a222bbd956cf2f40b85f9bc09c1ffdf
Abdul Kader <abdulkaderm36@gmail.com>
2022-08-12 03:56:24 +0400

    Added the Header and ShopScreen to the App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+
+//styles
 import './App.css'
+
+//Screens
+import ShopScreen from './screens/ShopScreen'
+
+//Components
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-        Dealerz
+      <Header/>
+      <ShopScreen/>
     </div>
   )
 }
